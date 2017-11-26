7da6253bfa9a8eb61fdb1fe5e34ae06d00ae43a3
Cesar Augusto <caherdenez@gmail.com>
2017-11-25 21:46:57 -0500

    reorganizing project

--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -11,20 +15,49 @@ func messageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to Go Web Development")
 }
 
+// upload logic
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	h := md5.New()
+	token := fmt.Sprintf("%x", h.Sum(nil))
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		fmt.Fprintf(w, token)
+
+	} else {
+		fmt.Fprintf(w, "POST")
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./public/"+token+".png", os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func main() {
 
-	http.HandleFunc("/to", messageHandler)
+	http.HandleFunc("/menssage", messageHandler)
+	http.HandleFunc("/upload", upload)
 	server := &http.Server{
 		Addr:           ":8080",
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	log.Println("Listening...")
+	log.Println("Listening... in the Port")
 
 	fs := http.FileServer(http.Dir("public"))
-	graphqil := http.FileServer(http.Dir("public/graphliq"))
 	http.Handle("/", fs)
-	http.Handle("/graphqil", graphqil)
 	server.ListenAndServe()
 }
